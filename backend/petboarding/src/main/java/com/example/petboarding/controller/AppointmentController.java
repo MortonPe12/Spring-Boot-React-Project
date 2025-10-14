500b73cdc2a443ff3db07652c9d4ebd4e442804b
MortonPe12 <petermorton1247@gmail.com>
2025-10-14 14:50:52 -0400

    Changed the CrossOrigin to include the AWS S3 url instead of localhost

--- backend/petboarding/src/main/java/com/example/petboarding/controller/AppointmentController.java
+++ backend/petboarding/src/main/java/com/example/petboarding/controller/AppointmentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://petprojectreactfrontend.s3-website.us-east-2.amazonaws.com")
 @RestController
 @RequestMapping("/appointments")
 public class AppointmentController {
